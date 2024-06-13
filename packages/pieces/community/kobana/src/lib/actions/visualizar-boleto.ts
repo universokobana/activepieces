7b6943c2c30382c94e2635b6024d53d0d94368f4
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-06-13 16:18:56 -0300

    feat: (test) visualize boleto

--- packages/pieces/community/kobana/src/lib/actions/visualizar-boleto.ts
+++ packages/pieces/community/kobana/src/lib/actions/visualizar-boleto.ts
@@ -1,22 +1,28 @@
 import { createAction, Property, PieceAuth } from '@activepieces/pieces-framework';
-import { httpClient, HttpMethod } from '@activepieces/pieces-common';
+import { httpClient, HttpMethod, HttpRequest } from '@activepieces/pieces-common';
 import { kobanaAuth } from '../..';
 
 export const visualizarBoleto = createAction({
-  // auth: check https://www.activepieces.com/docs/developers/piece-reference/authentication,
-  name: 'visualizarBoleto',
+  name: 'visualizar_boleto',
   auth: kobanaAuth,
   displayName: 'visualizar boleto',
   description: 'Consulta informacoes especificas de um boleto',
   props: {},
+
   async run(context) {
-    const res = await httpClient.sendRequest<string[]>({
+    const request: HttpRequest = {
       method: HttpMethod.GET,
       url: 'https://cloud.activepieces.com/api/v1/webhooks/RGjv57ex3RAHOgs0YK6Ja/sync',
       headers: {
-        Authorization: context.auth,
-      },
-    });
-    return res.body;
+        authorization: context.auth.access_token,
+        'Content-Type': 'application/json',
+      }
+    };
+
+    const res = await httpClient.sendRequest(request);
+
+    return {
+      success: res.status === 200,
+    }
   },
 });
