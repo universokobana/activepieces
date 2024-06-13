dc78acd2091eee2e9525a0231082882a9616ac93
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-06-13 16:16:23 -0300

    fix: authentication method

--- packages/pieces/community/kobana/src/index.ts
+++ packages/pieces/community/kobana/src/index.ts
@@ -1,12 +1,18 @@
-
 import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
 import { visualizarBoleto } from "./lib/actions/visualizar-boleto";
+import { bank_billet_created } from "./lib/triggers/bank_billet_created";
 
-export const kobanaAuth = PieceAuth.SecretText({
-  displayName: "API Key",
+export const kobanaAuth = PieceAuth.OAuth2({
+  description: '',
+  authUrl: 'https://api-sandbox.kobana.com.br/oauth/authorize',
+  tokenUrl: 'https://api-sandbox.kobana.com.br/oauth/token',
   required: true,
-  description: "Please use **test-key** as value for API key"
+  scope: [
+    'login',
+    'write'
+  ],
 });
+
 
 export const kobana = createPiece({
   displayName: "Kobana",
@@ -15,6 +21,5 @@ export const kobana = createPiece({
   logoUrl: "https://cdn.activepieces.com/pieces/kobana.png",
   authors: [],
   actions: [visualizarBoleto],
-  triggers: [],
+  triggers: [bank_billet_created],
 });
-    
