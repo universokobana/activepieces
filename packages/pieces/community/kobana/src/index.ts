7b260a7061a7e68e50b3ca1421b4ee3e1679e67d
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-06-18 13:13:51 -0300

    feat: add trigger imports, scope and auth / token urls

--- packages/pieces/community/kobana/src/index.ts
+++ packages/pieces/community/kobana/src/index.ts
@@ -1,25 +1,42 @@
-import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
-import { visualizarBoleto } from "./lib/actions/visualizar-boleto";
-import { bank_billet_created } from "./lib/triggers/bank_billet_created";
+import { createPiece, PieceAuth } from '@activepieces/pieces-framework';
+import { visualizarBoleto } from './lib/actions/visualizar-boleto';
+import { bank_billet_blocked } from './lib/triggers/bank_billet_blocked';
+import { bank_billet_canceled } from './lib/triggers/bank_billet_canceled';
+import { bank_billet_created } from './lib/triggers/bank_billet_created';
+import { bank_billet_generated } from './lib/triggers/bank_billet_generated';
+import { bank_billet_overdue } from './lib/triggers/bank_billet_overdue';
+import { bank_billet_paid } from './lib/triggers/bank_billet_paid';
+import { bank_billet_registered } from './lib/triggers/bank_billet_registered';
+import { bank_billet_rejected } from './lib/triggers/bank_billet_rejected';
+import { bank_billet_updated } from './lib/triggers/bank_billet_updated';
+
+export const kobanaHost =
+  process.env['KOBANA_APP_HOST'] || 'http://localhost:5000';
 
 export const kobanaAuth = PieceAuth.OAuth2({
   description: '',
-  authUrl: 'https://api-sandbox.kobana.com.br/oauth/authorize',
-  tokenUrl: 'https://api-sandbox.kobana.com.br/oauth/token',
+  authUrl: `${kobanaHost}/oauth/authorize`,
+  tokenUrl: `${kobanaHost}/oauth/token`,
   required: true,
-  scope: [
-    'login',
-    'write'
-  ],
+  scope: ['login', 'write'],
 });
 
-
 export const kobana = createPiece({
-  displayName: "Kobana",
+  displayName: 'Kobana',
   auth: kobanaAuth,
   minimumSupportedRelease: '0.20.0',
-  logoUrl: "https://cdn.activepieces.com/pieces/kobana.png",
+  logoUrl: 'https://cdn.activepieces.com/pieces/kobana.png',
   authors: [],
   actions: [visualizarBoleto],
-  triggers: [bank_billet_created],
+  triggers: [
+    bank_billet_blocked,
+    bank_billet_canceled,
+    bank_billet_created,
+    bank_billet_generated,
+    bank_billet_overdue,
+    bank_billet_paid,
+    bank_billet_registered,
+    bank_billet_rejected,
+    bank_billet_updated,
+  ],
 });
